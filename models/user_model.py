8b518768208d08b72f2deffbf541c220c6bbac7e
Rodrigo Martins <Rodrigo.MartinsFernandes@fit-tecnologia.org.br>
2022-07-29 15:32:42 -0400

    first commit

--- models/user_model.py
+++ models/user_model.py
@@ -2,7 +2,7 @@ from typing import Optional
 from sqlmodel import Field, SQLModel
 
 class UserModel(SQLModel, table=True):
-    __tablename__ = str = 'users'
+    __tablename__: str = 'users'
 
     id: Optional[int] = Field(default=None, primary_key=True)
     first_name: str
